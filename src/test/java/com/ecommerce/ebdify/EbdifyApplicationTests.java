99b1336021f440a1d1367b4d06852bc2c2b48bd6
Ebaad Azam <ebaadazam82881@gmail.com>
2025-04-12 13:05:52 +0530

    Initial commit

---
+++ src/test/java/com/ecommerce/ebdify/EbdifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ebdify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbdifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
