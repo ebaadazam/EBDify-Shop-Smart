768b5abcd64b90bdc590cafd50ce31ea3aa27071
Ebaad Azam <ebaadazam82881@gmail.com>
2025-04-19 15:56:23 +0530

    done with auth stuff, now will start cart module

--- src/main/java/com/ecommerce/ebdify/security/request/LoginRequest.java
+++ src/main/java/com/ecommerce/ebdify/security/request/LoginRequest.java
@@ -1,11 +1,17 @@
 package com.ecommerce.ebdify.security.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginRequest {
+    @NotBlank
     private String username;
+
+    @NotBlank
+    @Size(min = 8, message = "Password must be of at least 8 characters")
     private String password;
 }
