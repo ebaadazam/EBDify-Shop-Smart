223ae1e38f776f8265fad5fc2af33365e3a533c3
Ebaad Azam <ebaadazam82881@gmail.com>
2025-04-19 12:05:01 +0530

    introduced signin and signup endpoints

--- src/main/java/com/ecommerce/ebdify/security/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/ebdify/security/WebSecurityConfig.java
@@ -61,8 +61,8 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers("/v3/api-docs/**").permitAll()
-                                .requestMatchers("/api/admin/**").permitAll()
-                                .requestMatchers("/api/public/**").permitAll()
+//                                .requestMatchers("/api/admin/**").permitAll()
+//                                .requestMatchers("/api/public/**").permitAll()
                                 .requestMatchers("/swagger-ui/**").permitAll()
                                 .requestMatchers("/api/test/**").permitAll()
                                 .requestMatchers("/images/**").permitAll()
@@ -85,4 +85,5 @@ public class WebSecurityConfig {
                 "/swagger-ui.html",
                 "/webjars/**"));
     }
+
 }
---
+++ src/main/java/com/ecommerce/ebdify/security/response/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.ecommerce.ebdify.security.response;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+    private String message;
+}
